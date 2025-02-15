e88ff8f76d2e09d41d33a1fcc7999c7aa4ac91b0
Jiya Mehta <jiyamehta2110@gmail.com>
2025-02-15 13:30:24 +0530

    Hangman Game Project

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,62 @@
+const hangmanImage = document.querySelector(".hangman-box img");
 const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
 
-let currentWord;
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `hangman-${wrongGuessCount}.svg`;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join(""); 
+    gameModal.classList.remove("show");
+}
 
 const getRandomWord = () => {
     // selecting a random word and hint from wordList
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word);
     currentWord = word;
+    console.log(word);
     document.querySelector(".hint-text b").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+    resetGame();
+}
+
+const gameOver = (isVictory) => {
+    setTimeout(() => {
+        const modalText = isVictory ? `You found the word: ` : `The correct word was: `;
+        gameModal.querySelector("img").src = `${isVictory ? 'victory' : 'lost'}.gif`;
+        gameModal.querySelector("h4"),innerText = `${isVictory ? 'Congratulations!' : 'Game Over!'}`;
+        gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+        gameModal.classList.add("show");
+    }, 300);
 }
 
 const initGame = (button, clickedLetter) => {
     // checking if clickedLetter exists in the currentWord
     if(currentWord.includes(clickedLetter)) {
-        console.log(clickedLetter, " exists in the word");
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
     } else {
-        console.log(clickedLetter, " does not exist in the word")
+        // if clicked letter doesn't exist then update the wrongGuessCount and hangman image
+        wrongGuessCount++;
+        hangmanImage.src = `hangman-${wrongGuessCount}.svg`;
     }
+    button.disabled = true;
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    // calling gameOver function if any of these conditions is met
+    if(wrongGuessCount === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true); 
 }
 
 // creating keyboard buttons and adding event listeners
@@ -30,3 +68,4 @@ for(let i = 97; i <= 122; i++) {
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
